559e7779e8175c0e4f49e4729bb5b2ca3aff1ec4
10vital08 <vital.lushnikov@mail.ru>
2022-12-20 19:13:04 +0300

    Hooray

--- HomeWork_4/program.java
+++ HomeWork_4/program.java
@@ -4,45 +4,29 @@ import java.util.Scanner;
 
 public class program {
 
-    public static int FindCount(int a_, int b_) {
-        int count = 1;
-        int temp = 0;
-        for (int i = a_; i <= b_; i++) {
-            temp = i;
+    public static int[] FillArray (int start, int x){
+        int[] array = new int[x + 1];
 
-            while (b_ / temp >= 2) {
-                count++;
-                if (b_ + 1 / temp >= 2) {
-                    count++;
-                }
-                temp *= 2;
+        for (int i = 0; i < x + 1; i++) {
+            array[i] = 0;
+        }
+        array[start] = 1;
+
+        for (int i = start + 1; i < x + 1; i++) {
+            array[i] = array[i - 1];
+
+            if((i % 2 == 0) && (i / 2 >= start)){
+                array[i] += array[i / 2];
             }
         }
-        return count;
-    }
-
-    static int F(int a, int b) {
-        // System.out.println(b);
-        // new Scanner(System.in).nextLine();
-        if (b == a)
-            return 1;
-        else if (b < a) {
-            return 0;
-        } else if (b % 2 != 0)
-            return F(a, b - 1);
-        else
-            return F(a, b - 1) + F(a, b / 2);
+        return array;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = 2;
-        int b = 1000;
-
-        // int a = Integer.parseInt(sc.nextLine());
-        // int b = Integer.parseInt(sc.nextLine());
-        System.out.println(FindCount(a, b));
-        // System.out.println(F(2, 13));
-
+        int a = 10;
+        int b = 20;
+        int[] result = FillArray(a, b);
+        System.out.println(result[b]);
     }
 }
